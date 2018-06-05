608f1b715f7df36ca1960ec6039f536ac305ca03
semitro <semitro_8@mail.ru>
2018-06-05 20:04:11 +0300

    menu text

--- script.sh
+++ script.sh
@@ -1,2 +1,13 @@
-pwd | gawk -F '/' ' {print $NF}'
+function printMenu {
+	echo '1 - print name of current directory
+2 - change current directory
+3 - print date and time
+4 - print file content
+5 - copy file
+6 - exit'
+}
+function printCurrentDirectoryName(){
+	echo `pwd | gawk -F '/' ' {print $NF}'`
+}
 
+printMenu
