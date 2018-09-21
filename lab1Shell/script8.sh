09b0c8329375b70f446873e144c3ec68d97f7255
semitro <semitro_8@mail.ru>
2018-09-22 00:35:46 +0300

    refacting

--- lab1Shell/script8.sh
+++ lab1Shell/script8.sh
@@ -3,13 +3,25 @@
 RED='\033[1;31m'
 NC='\033[0m'
 err_file=$(echo ~/lab1_err)
-echo $err_file
-catchKill(){
+
+trap catchInt 2 
+catchInt(){
 	echo "The mafia was killed"
 	exit 0
 }
 
-trap catchKill 8 
+printError(){
+	echo -e "${RED}Error:$1$NC" 1>&2
+}
+
+# input an argument and execute command with it
+# if the arg is not zero
+read_exec(){
+	read file_name
+	if [ -n file_name ]; then
+		"$1" "$file_name" 
+	fi
+}
 
 printMenu(){
 	echo '1 - print name of current directory
@@ -24,52 +36,32 @@ printCurrentDirectoryName(){
 	pwd -P 2>$err_file || echo ":(" >&2
 }
 
-printError(){
-	echo -e "${RED}Error:$1$NC" 1>&2
-}
-
 changeDirectory(){
- 	( cd -- "$1" 2>> $err_file )
-	if [ $? -ne 0 ]
-	then
-		#echo -e "${RED}An error has ocurred$NC" 1>&2
-		echo "An error has ocurred" 1>&2
-	else
-		cd -- "$1"
-	fi
+ 	( cd -- "$1" 2>> $err_file ) &&
+	 cd -- "$1" || printError ":("
 } 
 
 createFile(){
-	echo $1
 	if [ -e "$1" ]; then
 		printError "file already exists"
 		return
 	fi
-	touch -- "$1" 2>> $err_file
-	if [ -n $? ]; then
-		echo "Eroor" 1>&2
-	fi
+	touch -- "$1" 2>> $err_file ||  printError "file :("
 }
 
 permitWritingToAll(){
-	chmod a+w "$1" >&2 2>> $err_file
+	chmod a+w "$1" >&2 2>> $err_file || printError ":("
 }
 
 destroyFile(){
 	if ! [ -e "$1" ]; then
-		#echo "safsf" 1>&2
 		return
 	fi
-	exec 5>/dev/pts/156
-	echo "rm: remove $1 (да/нет)?"
-	echo "rm: remove $1 (да/нет)?" >&5 
+	echo "rm: remove $1 (yes/no)?"
 	read confirm
-	if [ "да" = "$confirm" ] || [ "yes" = "$confirm" ] || [ "y" = "$confirm" ]; then	
-		 rm  -- "$1" 2>> $err_file || echo ":(" 
-		 if ! [ $0 -eq 0 ]; then
-			echo "no." 1>&2
-		fi
-
+	if  [ "yes" = "$confirm" ] || [ "y" = "$confirm" ]; then	
+		rm  -- "$1" 2>> $err_file || printError "rm :(" 
+		echo "done" >&2
 	fi
 }
 
@@ -82,35 +74,22 @@ mainLoop(){
 		1) printCurrentDirectoryName
 			;;
 		2) echo "where would you like to go? deeper?"
-			read file_name
-			if [ -n "$file_name" ]
-		 	then	
-				changeDirectory "$file_name"
-			fi
+			read_exec "changeDirectory"
 			;;
 		3) 	echo "Enter file name"
-			read file_name
-			echo "$file_name"
-			if [ -n "$file_name" ]; then
-				createFile "$file_name"
-			fi
+			read_exec "createFile"
 			;;
 		4) echo "Enter file name"
-			read file_name
-			if [ -n "$file_name" ]
-			then
-				permitWritingToAll $file_name
-			fi
+			read_exec "permitWritingToAll"
 			;;
 		5) echo "Which file do you want to destroy?"
-			read file_name
-			destroyFile "$file_name"
+			read_exec "destroyFile"
 			;;
 		6) break
 			;;
 		7|"ls") ls
 			;;
-		*) echo "Enter correct value" 
+		*) printError "Enter correct value" 
 			;;
        		esac	
 	printMenu
